e777c47432af383541a5b68ad8aa90d17939ef5e
Aakibvah <87711863+Aakibvah@users.noreply.github.com>
2023-02-17 22:08:28 -0500

    added form fields

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,26 @@
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+
 import {
+  //   auth,
   signInWithGooglePopup,
+  //   signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 const SignIn = () => {
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       const response = await getRedirectResult(auth);
+  //       if (response) {
+  //         const userDocRef = createUserDocumentFromAuth(response.user);
+  //       }
+  //     }
+  //     fetchData();
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = createUserDocumentFromAuth(user);
@@ -13,6 +30,10 @@ const SignIn = () => {
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
+     Sign in with Google Redirect
+       </button> */}
     </div>
   );
 };
